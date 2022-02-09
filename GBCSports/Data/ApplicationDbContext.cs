41d78fe4ecc82dc4e9f2b0a366a4bea3e5362d31
NamDo8467 <huynhnamdo@gmail.com>
2022-02-08 19:35:50 -0500

    Configure ApplicationDbContext

---
+++ GBCSports/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GBCSports.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GBCSports.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Incidents> Incidents { get; set; }
+
+        
+    }
+}
