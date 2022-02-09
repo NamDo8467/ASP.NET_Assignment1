4e522224bfefbbd961020e0dfc53a3b6add5a6f7
NamDo8467 <huynhnamdo@gmail.com>
2022-02-08 19:36:52 -0500

    Create Incidents table in the database

---
+++ GBCSports/Models/Incidents.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GBCSports.Models
+{
+    public class Incidents
+    {
+        public const string errorMessage = "Can not leave this field empty";
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string Product { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string Description { get; set; }
+
+        public string? Technician { get; set; }
+        public DateTime? DateOpened { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = errorMessage)]
+        public DateTime DateClosed { get; set; } 
+    }
+}
