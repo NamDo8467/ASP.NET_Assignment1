f59e8b359afa5d75c377ca230fba9f332dc3e973
NamDo8467 <huynhnamdo@gmail.com>
2022-02-10 13:24:54 -0500

    Create actions for Add, Index and Edit

---
+++ GBCSports/Controllers/IncidentsController.cs
@@ -0,0 +1,45 @@
+﻿using GBCSports.Data;
+using GBCSports.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GBCSports.Controllers
+{
+    public class IncidentsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IncidentsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Incidents> incidents = _db.Incidents.ToList();
+            return View(incidents);
+        }
+
+        public IActionResult Add()
+        {
+            Incidents incident = new Incidents();
+            ViewBag.Action = "Add";
+            return View("Add", incident);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var incident = _db.Incidents.Find(id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+
+            
+            return View("Add", incident);
+        }
+    }
+}
