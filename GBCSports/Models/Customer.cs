9991a7f46520d6460f1a29ffbc83325eccc7f437
NamDo8467 <huynhnamdo@gmail.com>
2022-02-10 13:25:32 -0500

    Create Customer class

---
+++ GBCSports/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GBCSports.Models
+{
+    public class Customer
+    {
+        const string errorMessage = "Can  not leave this field empty";
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string FirstName { get; set; } 
+        
+        [Required(ErrorMessage = errorMessage)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string Address { get; set; }
+        [Required(ErrorMessage = errorMessage)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string Country { get; set; }
+        
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+    }
+}
