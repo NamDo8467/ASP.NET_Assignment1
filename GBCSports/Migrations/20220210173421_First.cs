28e89017a19a623c88c31a6cfb85da2f3f0ffe16
minhnhat-auto <nhatv.eng@gmail.com>
2022-02-12 02:29:23 +0000

    Changed Setting

---
+++ GBCSports/Migrations/20220210173421_First.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GBCSports.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Code = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<long>(type: "bigint", nullable: false),
+                    Release_Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Code);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ GBCSports/Migrations/20220210174640_Second.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GBCSports.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Code", "Name", "Price", "Release_Date" },
+                values: new object[] { "TRNY10", "Tournament Master 1.0", 4L, new DateTime(2022, 2, 10, 17, 46, 40, 12, DateTimeKind.Local).AddTicks(4308) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Code",
+                keyValue: "TRNY10");
+        }
+    }
+}
