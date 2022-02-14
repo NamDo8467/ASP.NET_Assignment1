33537d614ce61f78417b70b67b475fece52aabdb
Meness428 <meness1234@gmail.com>
2022-02-14 13:46:54 -0500

    test1

---
+++ GBCSports/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GBCSports.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Technician> Technicians { get; set; }
+    }
+}
---
+++ GBCSports/Models/Technician.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GBCSports.Models
+{
+    public class Technician
+    {
+        [Key]
+        public int TechnicianId { get; set; }
+
+        [Required]
+        public string TechnicianName { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }  
+    }
+}
