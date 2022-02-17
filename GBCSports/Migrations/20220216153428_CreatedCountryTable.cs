865e2bba9e59aa73d7595650148cf956e05a3c9e
NamDo8467 <huynhnamdo@gmail.com>
2022-02-17 10:40:25 -0500

    Create Country table in db

---
+++ GBCSports/Migrations/20220216153428_CreatedCountryTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GBCSports.Migrations
+{
+    public partial class CreatedCountryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Canada" },
+                    { 2, "VietNam" },
+                    { 3, "United States of America" },
+                    { 4, "Russia" },
+                    { 5, "Italy" },
+                    { 6, "France" },
+                    { 7, "Spain" },
+                    { 8, "England" },
+                    { 9, "Germany" },
+                    { 10, "Netherlands" },
+                    { 11, "Poland" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
