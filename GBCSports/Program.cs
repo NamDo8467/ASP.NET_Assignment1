a76e0cc17e211b4b58339a603e1d1056b0257fca
NamDo8467 <huynhnamdo@gmail.com>
2022-02-23 14:51:26 -0500

    update settings

--- GBCSports/Program.cs
+++ GBCSports/Program.cs
@@ -38,8 +38,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-//app.MapControllerRoute(
-//    name: "technician",
-//    pattern: "{controller=Technician}/{action=List}/{id?}");
-
 app.Run();
