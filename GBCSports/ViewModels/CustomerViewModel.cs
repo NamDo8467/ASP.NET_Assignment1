bc2d0aa53c40c8e62f438e5272b17f3f1f372411
NamDo8467 <huynhnamdo@gmail.com>
2022-04-03 12:01:31 -0400

    Add viewmodels

---
+++ GBCSports/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GBCSports.Models
+{
+    public class CustomerViewModel
+    {
+        public List<string> CountryList { get; set;}
+        public Customer Customer { get; set;}
+    }
+}
---
+++ GBCSports/ViewModels/IncidentViewModel.cs
@@ -0,0 +1,15 @@
+﻿//using GBCSports.Data;
+using GBCSports.Models;
+
+namespace GBCSports.ViewModels
+{
+    public class IncidentViewModel
+    {
+        public List<Incident> incientList { get; set; }
+        public string filter { get; set; }
+        
+        public List<Customer> customerList { get; set; }
+
+               
+    }
+}
