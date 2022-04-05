09f2fccfd4d41e8f1eabc923781795c7fbfe2902
NamDo8467 <huynhnamdo@gmail.com>
2022-04-05 00:27:24 -0400

    Edit Customer class

--- GBCSports/Models/Customer.cs
+++ GBCSports/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+
 namespace GBCSports.Models
 {
     public class Customer
@@ -10,35 +11,45 @@ namespace GBCSports.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = errorMessage)]
+        [StringLength(50, MinimumLength = 1)]
         [Display(Name = "First Name")]
         public string FirstName { get; set; } 
         
         [Required(ErrorMessage = errorMessage)]
+        [StringLength(50, MinimumLength = 1)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
 
         [Required(ErrorMessage = errorMessage)]
+        [StringLength(50, MinimumLength = 1)]
         public string Address { get; set; }
 
 
         [Required(ErrorMessage = errorMessage)]
+        [StringLength(50, MinimumLength = 1)]
         public string City { get; set; }
 
 
         [Required(ErrorMessage = errorMessage)]
+        [StringLength(50, MinimumLength = 1)]
         public string State { get; set; }
 
 
         [Required(ErrorMessage = errorMessage)]
         [Display(Name = "Postal Code")]
+        [StringLength(20, MinimumLength = 1)]
         public string PostalCode { get; set; }
 
         [Required(ErrorMessage = errorMessage)]
         public string Country { get; set; }
-        
-        public string? Email { get; set; }
 
+        [StringLength(50, MinimumLength = 1)]
+        [DataType(DataType.EmailAddress)]
+        
+        public string Email { get; set; }
+
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
         public string? Phone { get; set; }
     }
 }
