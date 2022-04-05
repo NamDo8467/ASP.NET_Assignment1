dc7a25ad9bd6f897aa64974c8284a1db331fde35
NamDo8467 <huynhnamdo@gmail.com>
2022-04-04 21:44:22 -0400

    Add IncidentViewModel

--- GBCSports/ViewModels/IncidentViewModel.cs
+++ GBCSports/ViewModels/IncidentViewModel.cs
@@ -1,15 +1,40 @@
-﻿//using GBCSports.Data;
-using GBCSports.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace GBCSports.ViewModels
+namespace GBCSports.Models
 {
     public class IncidentViewModel
     {
-        public List<Incident> incientList { get; set; }
-        public string filter { get; set; }
-        
-        public List<Customer> customerList { get; set; }
+        public const string errorMessage = "Can not leave this field empty";
+        //public Incident CurrentIncident { get; set; }
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = errorMessage)]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string Product { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public string Description { get; set; }
+
+        public string? Technician { get; set; }
+
+        [Required(ErrorMessage = errorMessage)]
+        public DateTime? DateOpened { get; set; } = DateTime.Now;
+
+        public DateTime? DateClosed { get; set; }
+
+
+        
+       
+        public List<string> CustomerList { get; set; }
+
+        public List<string> ProductList { get; set; }
+
+        public List<string> TechnicianList { get; set; }
                
     }
 }
