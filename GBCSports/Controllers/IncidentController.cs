1dc02e8c83dc253587caa7cfd65af0eb1a50e9ca
NamDo8467 <huynhnamdo@gmail.com>
2022-04-04 21:46:36 -0400

    modify Incident

--- GBCSports/Controllers/IncidentController.cs
+++ GBCSports/Controllers/IncidentController.cs
@@ -1,5 +1,6 @@
 ﻿using GBCSports.Data;
 using GBCSports.Models;
+//using GBCSports.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GBCSports.Controllers
@@ -16,33 +17,80 @@ namespace GBCSports.Controllers
         [Route("/incidents")]
         public IActionResult List()
         {
+
+            IncidentManagerViewModel incidentViewModel = new IncidentManagerViewModel();
+            incidentViewModel.IncidentList = new List<Incident>();
             TempData["Incident"] = "text-white";
-            IEnumerable<Incident> incidents = _db.Incidents;
-            return View("Index", incidents);
+            incidentViewModel.Filter = HttpContext.Request.Query["Filter"].ToString();
+
+            if (incidentViewModel.Filter == "unassigned")
+            {
+                TempData["unassigned"] = "active";
+                incidentViewModel.IncidentList = _db.Incidents.Where(incident => incident.Technician == null).ToList();
+            }
+            else if (incidentViewModel.Filter == "open")
+            {
+                TempData["open"] = "active";
+                incidentViewModel.IncidentList = _db.Incidents.Where(incident => incident.DateClosed == null).ToList();
+            }
+            else
+                
+            {
+                TempData["all"] = "active";
+                incidentViewModel.IncidentList = _db.Incidents.ToList();
+            }
+            
+            return View("Index", incidentViewModel);
+        }
+
+        private void ConfigureViewModel(IncidentViewModel incidentViewModel)
+        {
+            
+            incidentViewModel.CustomerList = new List<string>();
+            incidentViewModel.CustomerList = _db.Customers.Select(customer => customer.FirstName + " " + customer.LastName).ToList();
+
+            incidentViewModel.ProductList = new List<string>();
+            incidentViewModel.ProductList = _db.Products.Select(product => product.Name).ToList();
+
+            incidentViewModel.TechnicianList = new List<string>();
+            incidentViewModel.TechnicianList = _db.Technicians.Select(technician => technician.FirstName + " " + technician.LastName).ToList();
         }
 
         public IActionResult Add()
         {
-            Incident incident = new Incident();
-            ViewBag.Customers = _db.Customers.ToList();
-            ViewBag.Products = _db.Products.ToList();
-            ViewBag.Technicians = _db.Technicians.ToList();
-            
-            return View("Add", incident);
+            IncidentViewModel incidentViewModel = new IncidentViewModel();
+            //Incident incident = new Incident();
+
+            ConfigureViewModel(incidentViewModel);
+
+            return View(incidentViewModel);
         }
 
         [HttpPost]
-        public IActionResult Add(Incident incident)
+        public IActionResult Add(IncidentViewModel incident)
         {
+            
             if (!ModelState.IsValid)
             {
-                ViewBag.Customers = _db.Customers.ToList();
-                ViewBag.Products = _db.Products.ToList();
-                ViewBag.Technicians = _db.Technicians.ToList();
+                
+                //TempData["m"] = incident.CustomerName;
+                ConfigureViewModel(incident);
                 return View(incident);
             }
+            Incident i = new Incident()
+            {
+                CustomerName = incident.CustomerName,
+                Product = incident.Product,
+                Title = incident.Title,
+                Description = incident.Description,
 
-            _db.Incidents.Add(incident);
+                Technician = incident.Technician,
+                DateOpened = incident.DateOpened,
+                DateClosed = incident.DateClosed,
+
+            };
+
+            _db.Incidents.Add(i);
             _db.SaveChanges();
             return RedirectToAction("List");
         }
@@ -55,33 +103,51 @@ namespace GBCSports.Controllers
             }
 
             var incident = _db.Incidents.Find(id);
-            ViewBag.Customers = _db.Customers.ToList();
-            ViewBag.Products = _db.Products.ToList();
-            ViewBag.Technicians = _db.Technicians.ToList();
+            
+            
             if (incident == null)
             {
                 return NotFound();
             }
+            
+            IncidentViewModel incidentViewModel = new IncidentViewModel();
+            
+            incidentViewModel.CustomerName = incident.CustomerName;
+            incidentViewModel.Product = incident.Product;   
+            incidentViewModel.Title = incident.Title;
+            incidentViewModel.Description = incident.Description;
+            incidentViewModel.Technician = incident.Technician;
+            incidentViewModel.DateOpened = incident.DateOpened;
+            incidentViewModel.DateClosed = incident.DateClosed;
+            ConfigureViewModel(incidentViewModel);
+
+            TempData["t"] = incidentViewModel.CustomerName;
 
 
-            return View("Edit", incident);
+            return View("Edit", incidentViewModel);
         }
 
         [HttpPost]
         public IActionResult Edit(Incident incident)
         {
+            IncidentViewModel incidentViewModel = new IncidentViewModel();
             if (!ModelState.IsValid)
             {
-                ViewBag.Customers = _db.Customers.ToList();
-                ViewBag.Products = _db.Products.ToList();
-                ViewBag.Technicians = _db.Technicians.ToList();
-               
-                return View("Edit", incident);
+                ConfigureViewModel(incidentViewModel);
+                incidentViewModel.CustomerName = incident.CustomerName;
+                incidentViewModel.Product = incident.Product;
+                incidentViewModel.Title = incident.Title;
+                incidentViewModel.Description = incident.Description;
+                incidentViewModel.Technician = incident.Technician;
+                incidentViewModel.DateOpened = incident.DateOpened;
+                incidentViewModel.DateClosed = incident.DateClosed;
+
+                return View("Edit", incidentViewModel);
             }
 
             _db.Incidents.Update(incident);
             _db.SaveChanges();
-            return RedirectToAction("List");
+            return RedirectToAction("List", incidentViewModel);
         }
 
         public IActionResult Delete(int? id)
